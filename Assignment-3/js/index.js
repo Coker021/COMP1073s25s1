6dd9da9a8d75b42e1673a1667f805d9ba34cf927
Taiwo Coker <taiwocoker021@gmail.com>
2025-08-05 17:10:40 -0400

    created an asynchronous function to fetch data from the weather api, added catch errors too

--- Assignment-3/js/index.js
+++ Assignment-3/js/index.js
@@ -16,6 +16,45 @@ const weatherResult = document.getElementById("weather-result");
 // STEP 5: Add an event listener to the button
 getWeatherBtn.addEventListener("click", getWeather);
 
+// STEP 6: Create the getWeather function
+async function getWeather() {
+    const city = cityInput.value;
+    const url = `${baseURL}?q=${city}&appid=${apiKey}&units=metric`;
+
+    // STEP 7: Fetch the weeather data from the API
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // STEP 8: Display the weather data
+            const temperature = data.main.temp;
+            const description = data.weather[0].description;
+            const humidity = data.main.humidity;
+            const windSpeed = data.wind.speed;
+            console.log(data);
+
+            const text = document.createElement("p");
+            text.textContent = `Temperature: ${temperature} °C, \n 
+                                Description: ${description}, \n
+                                Humidity: ${humidity}%, \n
+                                Wind Speed: ${windSpeed} m/s`;
+            weatherResult.appendChild(text);
+        })
+        .catch(error => {
+            // STEP 9: Handle errors
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = error.message;
+            weatherResult.appendChild(errorMessage);
+        });
+    console.log("Weather data fetched for city: " + city);
+
+
+}
+
 
 
    

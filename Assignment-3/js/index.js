5c99842ae773f59316567890d7f7c384c28d9d33
Taiwo Coker <taiwocoker021@gmail.com>
2025-08-05 16:24:06 -0400

    Created the Index view for the weather, also got the URL we'll be making the API calls too and the API-KEY

---
+++ Assignment-3/js/index.js
@@ -0,0 +1,15 @@
+// BASE URL: "https://api.openweathermap.org"
+
+// STEP 1: Store the API configuration
+// STEP 2: Set the base URL for the OpenWeatherMap API
+const baseURL = "https://api.openweathermap.org/data/2.5/weather";
+
+// STEP 3: Set your API key
+const apiKey = "b5da509689f5c4f193d5d8ae4c6dbe08";
+
+
+
+
+
+   
+

daff68b7e635d37e2e4411f9c8cce8d1b055515b
Taiwo Coker <taiwocoker021@gmail.com>
2025-08-05 16:55:47 -0400

    Defined the HTML elements

--- Assignment-3/js/index.js
+++ Assignment-3/js/index.js
@@ -7,6 +7,12 @@ const baseURL = "https://api.openweathermap.org/data/2.5/weather";
 // STEP 3: Set your API key
 const apiKey = "b5da509689f5c4f193d5d8ae4c6dbe08";
 
+// STEP 4: Define the HTML elements needed for interaction
+// These elements will be used to get user input and display results
+const cityInput  = document.getElementById("city");
+const getWeatherBtn = document.getElementById("get-weather");
+const weatherResult = document.getElementById("weather-result");
+
 
 
 
